69e44c4091686e51be08356fd3ee95bf1a87de98
Jason <ureadmyemailname@gmail.com>
2015-09-02 14:39:07 +0930

    Update milknation.js

--- milknation.js
+++ milknation.js
@@ -112,6 +112,17 @@ else {
 autoe = !autoe;
 API.sendChat("/me autoe message now set to " + e);}}};
 
+bot.commands.autof = {
+command: ['autof'],
+rank: 'manager',
+type: 'exact',
+functionality: function (chat, cmd) {
+if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+if (!bot.commands.executable(this.rank, chat)) return void (0);
+else {
+autof = !autof;
+API.sendChat("/me autof message now set to " + f);}}};
+
 bot.commands.a { 			
 command: 'a', 			
 rank: 'user', 			
